6e6cfbe8482d22577dab1f1b14c077bda75db09f
mohammadghareb <mohammadghareb35@gmail.com>
2023-05-09 13:10:52 +0300

    add todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
 import styled from "@emotion/styled";
-
+import { AddInput } from "./components/TodoList/AddInput";
+import { TodoItem } from "./components/TodoList/TodoItem";
+import { TodoList } from "./components/TodoList";
 import { Header } from "./components/Header";
-import "./index.css";
+import "./App.css";
+
 const Wrapper = styled.div({
   display: "flex",
   flexDirection: "column",
@@ -10,14 +14,60 @@ const Wrapper = styled.div({
   width: 300
 });
 
-const App = () => {
+const initialData = [
+  {
+    id: uuid(),
+    label: "Buy groceries",
+    checked: false
+  },
+  {
+    id: uuid(),
+    label: "Reboot computer",
+    checked: false
+  },
+  {
+    id: uuid(),
+    label: "Ace CoderPad interview",
+    checked: true
+  }
+];
+
+function App() {
+  const [todos, setTodos] = useState(initialData);
+
+  const addTodo = (label: string) => {
+    //implement adding another item to the list here
+    const newItem = {
+      id: uuid(),
+      label: label,
+      checked: false
+    };
+    setTodos([...todos, newItem]);
+    setTodos([newItem, ...todos]);
+
+  };
+
+  const handleChange = (target: any) => {
+    // handle the check/uncheck logic
+    const itemIndex = todos.findIndex((item) => item.id === target.id);
+    const newCheckedValue = !todos[itemIndex].checked;
+    if (newCheckedValue) {
+    }
+    todos[itemIndex].checked = newCheckedValue;
+    setTodos([...todos]);
+  };
 
   return (
     <Wrapper>
       <Header>Todo List</Header>
+      <AddInput onAdd={addTodo} />
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem {...todo} onChange={handleChange} />
+        ))}
+      </TodoList>
     </Wrapper>
   );
 }
 
 export default App;
-
